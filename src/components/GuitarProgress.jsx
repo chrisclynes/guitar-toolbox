20b246af109f5eff52b8abf478011e56f60b60b5
Chris Clynes <chrisclynes@live.com>
2022-04-18 23:51:04 -0400

    first commit

---
+++ src/components/GuitarProgress.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const GuitarProgress = () => {
+    return (
+        <div>
+            Progress area
+        </div>
+    );
+}
+
+export default GuitarProgress;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+        <div className="text-3xl bg-red-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-50 dark:text-gray-900">
+            NAVBAR
+        </div>
+    );
+}
+
+export default Navbar;

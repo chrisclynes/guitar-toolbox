c678d28691f803836a06d30d5720f391adcceee5
Chris Clynes <chrisclynes@live.com>
2022-05-20 22:23:40 -0400

    added antd selectors to chordProgressions styling change

--- src/components/ChordProgressions/ChordProgressions.jsx
+++ src/components/ChordProgressions/ChordProgressions.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Typography } from 'antd';
+import { Typography, Space, Button, Select} from 'antd';
 
 import { majorKeys, minorKeys, majorProgressions, minorProgressions } from '../../constants/data';
 import "./ChordProgressions.css";
+
+const { Option } = Select;
 
 const ChordProgressions = () => {
     const [majorMinor, setMajorMinor] = useState("Major");
@@ -24,44 +26,46 @@ const ChordProgressions = () => {
                 </div>
             </div>
             <div className="progression-selectors-container center-items">
-                <select className="major-minor-selction" name="Major Minor" id="major-minor-selctor" onChange={(e) => setMajorMinor(e.target.value)}>
-                    <option value="Major">Major</option>
-                    <option value="Minor">Minor</option>
-                </select>
-                {majorMinor === "Major" && (
-                    <div>
-                        <select className="major-keys-selction" name="Major Keys" id="major-keys-selctor" >
-                            {majorKeys.map((item) => {
-                                return (
-                                    <option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</option>
-                                )
-                            })}
-                        </select>
-                        <select className="major-progressions-selction" name="Major Progressions" id="major-progression-selctor" >
-                            {majorProgressions.map((item) => {
+                <Space size="small">
+                    <Select defauleValue="Major" size="autoSize" id="major-minor-selctor" onChange={(e) => setMajorMinor(e.target.value)}>
+                        <Option value="Major">Major</Option>
+                        <Option value="Minor">Minor</Option>
+                    </Select>
+                    {majorMinor === "Major" && (
+                        <div>
+                            <Select defauleValue="" size="autoSize" id="major-keys-selctor" >
+                                {majorKeys.map((item) => {
                                     return (
-                                        <option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</option>
+                                        <Option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</Option>
                                     )
                                 })}
-                        </select>
-                    </div>)}
-                {majorMinor === "Minor" && (
-                    <div className="minor-selection-container">
-                        <select className="minor-keys-selction" name="Minor Keys" id="minor-keys-selctor" >
-                            {minorKeys.map((item) => {
-                                return (
-                                    <option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</option>
-                                )
-                            })}
-                        </select>
-                        <select className="minor-progressions-selction" name="Minor Progressions" id="minor-progression-selctor" >
-                            {minorProgressions.map((item) => {
+                            </Select>
+                            <Select defauleValue="" size="autoSize" id="major-progression-selctor" >
+                                {majorProgressions.map((item) => {
+                                        return (
+                                            <Option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</Option>
+                                        )
+                                    })}
+                            </Select>
+                        </div>)}
+                    {majorMinor === "Minor" && (
+                        <div className="minor-selection-container">
+                            <Select defauleValue="" size="autoSize" id="minor-keys-selctor" >
+                                {minorKeys.map((item) => {
                                     return (
-                                        <option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</option>
+                                        <Option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</Option>
                                     )
                                 })}
-                        </select>
+                            </Select>
+                            <Select defauleValue="" size="autoSize" id="minor-progression-selctor" >
+                                {minorProgressions.map((item) => {
+                                        return (
+                                            <Option key={Object.keys(item)} value={Object.keys(item)}>{Object.keys(item)}</Option>
+                                        )
+                                    })}
+                        </Select>
                     </div>)}
+                </Space>
             </div>
         </div>
     )

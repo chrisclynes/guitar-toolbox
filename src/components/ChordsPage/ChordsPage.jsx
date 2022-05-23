3f7e9978f5d73fbb4490b24cfe4f4cb8c40888a2
Chris Clynes <chrisclynes@live.com>
2022-05-22 23:46:55 -0400

    made data arrays for selectors and have working state updates from ant d Selectors now

--- src/components/ChordsPage/ChordsPage.jsx
+++ src/components/ChordsPage/ChordsPage.jsx
@@ -15,11 +15,14 @@ const ChordsPage = ({theme}) => {
     const [chordData, setChordData] = useState({chordName: "A", strings: "X 0 2 2 2 0" });
     const [selectorVals, setSelectorVals] = useState({root: "A", quality: "", alterations: ""})
     
+    const chordKeySelectors = ["A_", "Ab_", "A%23_", "B_", "Bb_", "C_", "C%23_", "D_", "Db_", "D%23_", "E_", "Eb_", "F_", "F%23_", "G_", "Gb_", "G%23_"];
+    const chordQualitySelectors = ["", "major", "m", "dim"];
+
+    const handleSelectorsInput = (value) => {
+        setSelectorVals((prevState) => ({...prevState, root: value}))
+    }
 
     const handleChordData = async () => {
-        // const chordRoot = document.getElementById("chord-root-selector").value;
-        // const chordQuality = document.getElementById("chord-quality-selector").value;
-        // const chordAlternative = document.getElementById("chord-alterations").value;
         const URL = 'https://api.uberchord.com/v1/chords/';
 
         const chordToCall = `${URL}${selectorVals.root}${selectorVals.quality}${selectorVals.alterations}`;
@@ -51,31 +54,15 @@ const ChordsPage = ({theme}) => {
                 <ChordCard chordData={chordData} />
                 <div className="chord-options-container">
                     <Space size="small">
-                        <Select defaultValue="A" style={{width: "60px"}} name="chord-root-selector" onChange={(e) => {setSelectorVals(selectorVals.root = e.target.value)}}> 
-                            <Option value="A_">A</Option>
-                            <Option value="Ab_">Ab</Option>
-                            <Option value="A%23_">A#</Option>
-                            <Option value="B_">B</Option>
-                            <Option value="Bb_">Bb</Option>
-                            <Option value="C_">C</Option>
-                            <Option value="C%23_">C#</Option>
-                            <Option value="D_">D</Option>
-                            <Option value="Db_">Db</Option>
-                            <Option value="D%23_">D#</Option>
-                            <Option value="E_">E</Option>
-                            <Option value="Eb_">Eb</Option>
-                            <Option value="F_">F</Option>
-                            <Option value="F%23_">F#</Option>
-                            <Option value="G_">G</Option>
-                            <Option value="Gb_">Gb</Option>
-                            <Option value="G%23_">G#</Option>
+                        <Select defaultValue="A" style={{width: "60px"}} name="chord-root-selector" onChange={(val) => handleSelectorsInput(val)}>
+                            {chordKeySelectors.map((item, i) => {
+                                   return <Option key={i} value={item}>{item.replace(/(%23)/g, "#").replace(/(_)/g, '')}</Option>
+                               })}
                         </Select>
-                        <Select defauleValue="" style={{width: "70px"}} name="chord-quality-selector" onChange={(e) => {setSelectorVals(selectorVals.quality = e.target.value)}}>
-                            <Option value=""></Option>
-                            <Option value="maj">maj</Option>
-                            <Option value="m">m</Option>
-                            <Option value="dim">dim</Option>
-                            <Option value="%235">#5</Option>
+                        <Select defauleValue="" style={{width: "80px"}} name="chord-quality-selector" onChange={(e) => console.log(e)}>
+                            {chordQualitySelectors.map((item, i) => {
+                                   return <Option key={i} value={item}>{item}</Option>
+                               })}
                         </Select>
                         <Input style={{ width: "100px" }} name="chord-alterations" placeholder="sus2, maj9..." onChange={(e) => {setSelectorVals(selectorVals.alterations = e.target.value)}}/>
                         <Button type="primary" size="medium" onClick={() => handleChordData()} >Get Chord</Button>

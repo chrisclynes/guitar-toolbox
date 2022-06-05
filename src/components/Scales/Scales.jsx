ad38dc7e8ff3845bef70590f3aa75d8adbdae3fa
Chris Clynes <chrisclynes@live.com>
2022-06-04 23:56:52 -0400

    setup scales page and scales cards, added images which will be replaced later

--- src/components/Scales/Scales.jsx
+++ src/components/Scales/Scales.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography, Space, Button, Select, Row, Col, List} from 'antd';
+
+import { guitarScalesData } from '../../constants/data';
 
 import "./Scales.css";
 
 const Scales = () => {
     return (
-        <div>
-            Scales
+        <div className="chord-main-container">
+            <div className="chords-title">
+                <Typography.Title>Guitar Scale Lookup</Typography.Title>
+                <h2>Learn new scales and practice with a metronome</h2>
+            </div>
         </div>
     )
 }
---
+++ src/container/ScaleCard/ScaleCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from "antd";
+
+
+const ScaleCard = ({chordName, strings, title}) => {
+    return (
+        <Card title={title} style={{ margin: "1rem"}} bodyStyle={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            
+        </Card>
+    )
+}
+
+export default ScaleCard;

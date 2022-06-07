e502b52d03799ea05d9ec31b26ed65f2c833045b
Chris Clynes <chrisclynes@live.com>
2022-06-06 23:25:15 -0400

    re-worked metronome function, created a more accurate time js file that keeps time better, but still not perfect. may need to use an external library for this.

--- src/container/Metronome/Metronome.jsx
+++ src/container/Metronome/Metronome.jsx
@@ -4,51 +4,51 @@ import { Typography, Card, Space, Button, Select, Slider} from 'antd';
 import clickSound from '../../Sounds/clickSound.wav';
 import beatSound from '../../Sounds/beatSound.wav';
 
+import AccurateTime from "../../services/acuTimer";
+
 const click = new Audio(clickSound);
 const beat = new Audio(beatSound);
+
 
 const Metronome = () => {
     const [metronomeData, setMetronomeData] = useState({
         bpm: 90,
         count: 0,
-        measure: 4
+        measure: 3
     });
     const [isPlaying, setIsPlaying] = useState(false); 
     const { bpm, count, measure } = metronomeData;//destructure state
 
-    useEffect(() => {
-        let counter = count;//uses vals from state but state not updated each time. run within this effect only
-            const time = setInterval(() => {
-                if(isPlaying) {
-                    if(counter == 0) {
-                        beat.play();
-                    }else {
-                        click.play();
-                    }
-                   counter = (counter + 1) % measure
-                   console.log(counter)
-                }
-            }, (60000 / bpm));
-            return () => clearInterval(time);//clears interval if isPlaying changes back to false
-    }, [isPlaying, metronomeData])
+    const playClick = () => {
+        // console.log('here')
+        // if(count == 0){
+            beat.play()
+        // }else {
+        //     click.play()
+        // }
+        // setMetronomeData((prevState) => ({
+        //     ...prevState,
+        //     count: (count + 1) % measure
+        // }))
+    }
 
+    const clickTime = new AccurateTime(() => beat.play(), (60000 / bpm))
     const handleStartStopClick = () => {
-        if(isPlaying) {
-            setIsPlaying(false)      
+        if(!isPlaying){
+            setIsPlaying(true)
+            clickTime.start()
         }else {
-            setMetronomeData((prevState) => ({
-                ...prevState,
-                count: 0,
-            }))
-            setIsPlaying(true);
+            clickTime.stop()
+            setIsPlaying(false)
         }
+        
     }
     
     return (
         <Card title="Metronome" style={{width: "280px", margin: "1rem"}} bodyStyle={{display: "flex", justifyContent: "center", alignItems: "center"}}>
                 <Space style={{flexDirection: "column"}}>
                     <Typography.Title>{`${bpm} bpm`}</Typography.Title>
-                    <Slider min={0} max={300} defaultValue={90} style={{width: "200px"}} onChange={(val) => {setMetronomeData((prevState) => ({...prevState, bpm: val}))}}/>
+                    <Slider min={30} max={300} defaultValue={90} style={{width: "200px"}} onChange={(val) => {setMetronomeData((prevState) => ({...prevState, bpm: val}))}}/>
                     {!isPlaying &&
                         <Button label="start" onClick={() => handleStartStopClick()}>Start</Button>
                     }

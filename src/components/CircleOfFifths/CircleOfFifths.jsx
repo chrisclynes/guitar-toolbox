aea022ba75aaa89a99a59038b0877d0114ea86ec
Chris Clynes <chrisclynes@live.com>
2022-08-29 21:30:53 -0400

    creating circle, css for circle setup

--- src/components/CircleOfFifths/CircleOfFifths.jsx
+++ src/components/CircleOfFifths/CircleOfFifths.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Card, Image } from 'antd';
+import { Card } from 'antd';
 
 
 import './CircleOfFifths.css';
+const circleOutside = ["C", "G", "D", "A", "E", "B", "Gb", "Db", "Ab", "Eb", "Bb", "F"];
+
 
 const CircleOfFifths = ({ title, image}) => {
     return (
         <Card title={title} style={{ margin: "1rem"}} bodyStyle={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Image
-                width={250}
-                src={image}
-                alt="scale img"
-            />
+            <div className="outer-circle">
+                {circleOutside.map((item, i) => {
+                    return (
+                        <div key={i} className={`outsideSlice${i}`}>{item}</div>
+                    )
+                })}
+            </div>
+            <div className="inner-circle"></div>
         </Card>
     )
 }
